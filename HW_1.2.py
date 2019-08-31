945f3f7fa535231d1c75a16d7d72377618ee96df
Anry <54634437+AnryChe@users.noreply.github.com>
2019-08-31 17:42:28 +0300

    Delete HW_1.2.py

--- HW_1.2.py
+++
@@ -1,7 +0,0 @@
-while True:
-    number = int(input("Введите число от 1 до 10:"))
-    if  number >0 and number < 10 :
-        print(number*2)
-        break
-    else:
-        print("Число вне диапазона. Введите число от 1 до 10")
